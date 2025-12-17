84db2d1ca66ca30ce80d98ff76db654534a223c1
Saryn7373 <winningson90@gmail.com>
2025-12-18 01:15:57 +0300

    lab4 done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signin() {
+        return view("auth.signin");
+    }
+
+    public function register(Request $request) {
+        $user = $request->validate([
+            "name" => 'required',
+            "email" => "email | required",
+            "password" => "required | min:6"
+        ]);
+        return response()->json($user);
+    }
+}
---
+++ resources/views/main/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, consectetur!</p>
+@endsection
